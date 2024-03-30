47e3e610291dd6a531b13de248a7751ad77ac66f
Sunkyo Yoon <yoonsunkyo@gmail.com>
2024-03-30 13:23:16 -0400

    added q13

--- 0013-roman-to-integer.py
+++ 0013-roman-to-integer.py
@@ -16,3 +16,27 @@ class Solution:
                 ans += 40
                 skip = True
             elif (i+1) <= len(s) - 1 and s[i] == "X" and s[i+1] == "C":
+                ans += 90
+                skip = True
+            elif (i+1) <= len(s) - 1 and s[i] == "C" and s[i+1] == "D":
+                ans += 400
+                skip = True
+            elif (i+1) <= len(s) - 1 and s[i] == "C" and s[i+1] == "M":
+                ans += 900
+                skip = True
+            else:
+                if s[i] == "M":
+                    ans += 1000 
+                elif s[i] == "D":
+                    ans += 500 
+                elif s[i] == "C":
+                    ans += 100 
+                elif s[i] == "L":
+                    ans += 50
+                elif s[i] == "X":
+                    ans += 10
+                elif s[i] == "V":
+                    ans += 5 
+                elif s[i] == "I":
+                    ans += 1 
+        return ans
