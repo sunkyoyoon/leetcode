fa9dfd9f6a005cd147d6bca332eef568747aedaa
justinyoonsk <justinyoonsk@gmail.com>
2024-04-24 12:53:45 -0400

    fixed q10

--- 0010-regular-expression-matching.py
+++ 0010-regular-expression-matching.py
@@ -1,19 +1,25 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        memo = defaultdict(bool)
-
-        def dfs(i, j):
+        memo = {} 
+        def dfs(i,j):
             if (i,j) in memo:
                 return memo[(i,j)]
-            if i > len(s)-1 and j > len(p)-1:
-                return True
-            if j > len(p)-1:
+            if i >= len(s) and j >= len(p):
+                return True 
+            if j >= len(p):
                 return False
+            
+            if j + 1 <= len(p) - 1 and p[j+1] == "*":
+                memo[(i,j)] = dfs(i,j+2) or (i <= len(s) - 1 and (s[i] == p[j] or p[j] == ".") and dfs(i+1,j))
+                return memo[(i,j)]
 
-            if i <= len(s)-1 and s[i] == p[j]:
-                memo[(i,j)] = memo[(i,j)] or dfs(i+1,j+1)
-            if p[j] == "." and (j+1) <= len(p)-1 and p[j+1] == "*":
-                if i <= len(s) - 1:
-                    memo[(i,j)] = memo[(i,j)] or dfs(i+1,j) or dfs(i+1,j+2)
-                else:
-                    memo[(i,j)] = memo[(i,j)] or df
+            if i <= len(s) - 1 and (s[i] == p[j] or p[j] == "."):
+                memo[(i,j)] = dfs(i+1,j+1)
+            else:
+                memo[(i,j)] = False 
+            return memo[(i,j)]
+
+        return dfs(0,0)
+
+# O(SP) time 
+# O(SP) space
