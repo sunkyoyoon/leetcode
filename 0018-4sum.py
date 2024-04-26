b7e8cea819e078659b2ca57d03b9e5b61a9c4667
Sun Kyo Yoon <yoonsunkyo@gmail.com>
2024-04-26 11:50:40 -0400

    fixed q18

--- 0018-4sum.py
+++ 0018-4sum.py
@@ -2,26 +2,30 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         ans = [] 
-        def kSum(k, l, curr, target):
+        def backtrack(k, curr, left, target):
             if k == 2:
-                r = len(nums) - 1 
-                while l < r:
-                    if nums[l] + nums[r] == target:
-                        ans.append(curr.copy()+[nums[l], nums[r]])
-                        l += 1
-                        while l < r and nums[l] == nums[l-1]:
-                            l += 1 
-                    elif nums[l] + nums[r] > target:
-                        r -= 1 
+                right = len(nums) - 1 
+                while left < right:
+                    if nums[left] + nums[right] == target:
+                        ans.append(curr + [nums[left], nums[right]])
+                        left += 1 
+                        right -= 1 
+                        while left < right and nums[left] == nums[left-1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right+1]:
+                            right -= 1 
+
+                    elif nums[left] + nums[right] > target:
+                        right -= 1 
                     else:
-                        l += 1
+                        left += 1 
                 return 
-            for i in range(l,len(nums)):
-                if i > l and nums[i] == nums[i-1]:
+            for i in range(left,len(nums)):
+                if i > left and nums[i] == nums[i-1]:
                     continue
                 curr.append(nums[i])
-                kSum(k-1,i+1,curr,target-nums[i])
+                backtrack(k-1, curr, i + 1, target - nums[i])
                 curr.pop()
 
-        kSum(4, 0, [], target)
+        backtrack(4, [], 0, target)
         return ans
