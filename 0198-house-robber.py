7b66e3ffe52ef1b9b8d25ce9c8f25569aae9a9f3
Sunkyo Yoon <yoonsunkyo@gmail.com>
2024-05-12 20:56:21 -0400

    modified q198

--- 0198-house-robber.py
+++ 0198-house-robber.py
@@ -1,16 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0 
-        if len(nums) == 1:
-            return nums[0]
-        if len(nums) == 2:
-            return max(nums)
-
-        for i in range(len(nums)):
-            if i == 2:
-                nums[i] = nums[i] + nums[i-2]
-            if i > 2:
-                nums[i] = nums[i] + max(nums[i-2], nums[i-3])
+        memo = {} 
+        def helper(i):
+            if i < 0:
+                return 0 
+            if i in memo:
+                return memo[i]
+            memo[i] = max(nums[i] + helper(i-2), helper(i-1))
+            return memo[i]
         
-        return max(nums[len(nums)-2], nums[len(nums)-1])
+        return helper(len(nums)-1)
