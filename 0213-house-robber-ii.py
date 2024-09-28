732cda71d2d13e8adc5d35527e09560bba57c3b9
Sunkyo Yoon <yoonsunkyo@gmail.com>
2024-09-28 16:27:06 -0400

    added q213

--- 0213-house-robber-ii.py
+++ 0213-house-robber-ii.py
@@ -1,14 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 3:
+        if len(nums) == 1 or len(nums) == 2 or len(nums) == 3:
             return max(nums)
-        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
-    
-    def helper(self, nums):
-        left = 0 
-        right = 0 
-        for i in range(len(nums)):
-            nums[i] = max(nums[i] + left, right)
-            left = right 
-            right = nums[i]
-        return nums[-1]
+
+        t1 = nums[len(nums) - 2]
+        t2 = nums[len(nums) - 1]
+        ans = 0 
+        for i in range(len(nums)-3):
+            ans = max(nums[i] + t1, t2)
+            if t1 < t2:
+                t1 = t2
+            t2 = ans
+        
+        ans2 = 0 
+        t1 = 0 
+        t2 = nums[len(nums)-1]
+        for i in range(len(nums)-2):
+            ans2 = max(nums[i] + t1, t2)
+            t1 = t2 
+            t2 = ans2 
+
+        return max(ans, ans2)
