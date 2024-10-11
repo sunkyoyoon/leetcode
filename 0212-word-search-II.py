c7ce6b62fff6968dc9031bf9a2c1a9debbc8f1b9
Sunkyo Yoon <yoonsunkyo@gmail.com>
2024-10-11 19:21:27 -0400

    added q212

---
+++ 0212-word-search-II.py
@@ -0,0 +1,52 @@
+class TrieNode:
+    def __init__(self):
+        self.end = False 
+        self.children = {}
+    
+class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        for w in words:
+            curr = self.root
+            for c in w:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.end = True  
+        root = self.root
+        ROWS, COLS = len(board), len(board[0])
+        res, visit = set(), set()
+
+        def dfs(r, c, node, word):
+            if (
+                r not in range(ROWS) 
+                or c not in range(COLS)
+                or board[r][c] not in node.children
+               
+                or (r, c) in visit
+            ):
+                return
+
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.end:
+                node.end = False
+                res.add(word)
+
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r, c + 1, node, word)
+            dfs(r, c - 1, node, word)
+            visit.remove((r, c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, "")
+
+        return list(res)
+
+                
+
