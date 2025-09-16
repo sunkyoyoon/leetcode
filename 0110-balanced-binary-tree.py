f1b36fc12cb5769a9c7990b5abfe39056b106603
Sun Kyo Yoon <yoonsunkyo@gmail.com>
2025-09-15 20:24:13 -0400

    added 0110 question

---
+++ 0110-balanced-binary-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if node is None:
+                return [True, 0]
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if abs(left[1] - right[1]) > 1:
+                left[0] = False
+
+            return [left[0] and right[0], max(left[1],right[1]) + 1] 
+
+        return dfs(root)[0]
