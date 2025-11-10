0316404014ca404e4aed3095c2864bda72691a65
Sun Kyo Yoon <yoonsunkyo@gmail.com>
2025-11-10 04:45:18 -0500

    added q2771

---
+++ 2771-longest-non-decreasing-subarray-from-two-arrays.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxNonDecreasingLength(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[1, 1] for _ in range(len(nums1) + 1)]
+        for i in range(1,len(nums1)):
+            if nums1[i] >= nums1[i-1]:
+                dp[i][0] = max(dp[i][0], dp[i-1][0] + 1)
+            if nums1[i] >= nums2[i-1]:
+                dp[i][0] = max(dp[i][0], dp[i-1][1] + 1) 
+            if nums2[i] >= nums1[i-1]:
+                dp[i][1] = max(dp[i][1], dp[i-1][0] + 1)
+            if nums2[i] >= nums2[i-1]:
+                dp[i][1] = max(dp[i][1], dp[i-1][1] + 1)
+        
+        return max(max(row) for row in dp)
+
