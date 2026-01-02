e53a83e66c1fa834291b029a65482dcb6d848ab2
Sun Kyo Yoon <yoonsunkyo@gmail.com>
2026-01-02 01:21:26 -0500

    added q44

--- 0044-wildcard-matching.py
+++ 0044-wildcard-matching.py
@@ -1,24 +1,30 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         memo = {} 
-        def dfs(i,j):
+        if s and p and s[-1] != p[-1] and p[-1] != "*" and p[-1] != "?":
+            return False
+        def dfs(i, j):
             if (i,j) in memo:
                 return memo[(i,j)]
-            if i >= len(s) and j >= len(p):
+            if i == len(s) and j == len(p):
                 return True 
-            if j >= len(p):
-                return False 
-
-            if p[j] == "*":
-                memo[(i,j)] = (i + 1 <= len(s) and dfs(i+1,j)) or dfs(i+1,j+1) or dfs(i,j+1)
-
-            elif i <= len(s) - 1 and (s[i] == p[j] or p[j] == "?"):
-                memo[(i,j)] = dfs(i+1,j+1)
+            elif i == len(s) and j < len(p):
+                while j < len(p) and p[j] == "*":
+                    j += 1 
+                if j == len(p):
+                    return True 
+                else:
+                    return False
+            elif i < len(s) and j == len(p):
+                return False
             
+            if s[i] == p[j] or p[j] == "?":
+                memo[(i,j)] = dfs(i+1, j+1)
+            elif p[j] == "*":
+                memo[(i,j)] = dfs(i, j+1) or dfs(i+1, j)
             else:
                 memo[(i,j)] = False
-
             return memo[(i,j)]
+                
 
         return dfs(0,0)
-
