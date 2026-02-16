a0bc6e8d1aa565fca962948fd21d3fa97f24964a
Sun Kyo Yoon <yoonsunkyo@gmail.com>
2026-02-16 02:21:03 -0500

    added q684

--- 0684-redundant-connection.py
+++ 0684-redundant-connection.py
@@ -1,21 +1,43 @@
+       class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {} 
+
+    def add(self, x):
+        if x not in self.parent:
+            self.parent[x] = x 
+            self.rank[x] = 0 
+
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX == rootY:
+            return False 
+
+        if self.rank[rootX] > self.rank[rootY]:
+            self.parent[rootY] = rootX
+        elif self.rank[rootY] > self.rank[rootX]:
+            self.parent[rootX] = rootY
+        else:
+            self.parent[rootY] = rootX
+            self.rank[rootX] += 1 
+        return True 
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        graph = collections.defaultdict(set)
-        seen = set() 
-        def dfs(u,v):
-            if u == v:
-                return True 
-            if u in seen:
-                return False 
-            seen.add(u)
-            for dst in graph[u]:
-                if dfs(dst, v):
-                    return True 
-            seen.remove(u)
-
+        uf = UnionFind()
         for u,v in edges:
-            if dfs(u,v):
+            uf.add(u)
+            uf.add(v)
+
+            if uf.find(u) == uf.find(v):
                 return [u,v]
-            graph[u].add(v)
-            graph[v].add(u)
-        
+            uf.union(u, v)
+
+        return 
+         
