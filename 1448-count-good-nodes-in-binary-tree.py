b705c789456c94d4448491f43fbe2026205d0542
Sun Kyo Yoon <yoonsunkyo@gmail.com>
2026-02-17 14:23:57 -0500

    add q1448

---
+++ 1448-count-good-nodes-in-binary-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        ans = 0 
+        def dfs(node, max_node):
+            nonlocal ans
+            if node == None:
+                return 
+
+            if max_node == None:
+                max_node = node.val
+            elif node.val >= max_node:
+                max_node = node.val
+                ans += 1
+
+            left = dfs(node.left, max_node)
+            right = dfs(node.right, max_node)
+
+        dfs(root, root.val)
+        return ans
+
