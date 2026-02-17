77f083ba4cad29ed55a836ef5278b1558e8661eb
Sun Kyo Yoon <yoonsunkyo@gmail.com>
2026-02-17 02:20:06 -0500

    add 69

---
+++ 0069-sqrt-x.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 0 
+        right = x 
+        while left < right:
+            mid = (left + right + 1) // 2
+            if mid * mid > x:
+                right = mid - 1 
+            else:
+                left = mid 
+
+        return left 
+
+
